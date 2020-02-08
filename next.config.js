ae1216f1c0f6262c15a7d3f4d921d6940f0d7afd
MeanYou <464595889@qq.com>
2020-02-08 17:03:22 +0800

    试试能不能访问out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? './' : '',
+    target: 'serverless',
 }
